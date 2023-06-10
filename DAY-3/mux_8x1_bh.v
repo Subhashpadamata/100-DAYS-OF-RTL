0e35b348f9b990d8fdf4a3c12b597878df79a418
PADAMATA SUBHASH <103590978+Subhashpadamata@users.noreply.github.com>
2023-06-10 22:44:17 +0530

    Add files via upload

---
+++ DAY-3/mux_8x1_bh.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+module mux_8x1_bh(
+    output reg y,
+    input [7:0]i,[2:0]s
+    );
+    
+    always @(*)
+    begin
+    case(s)
+        3'b000:y=i[0];
+        3'b001:y=i[1];
+        3'b010:y=i[2]; 
+        3'b011:y=i[3];
+        3'b100:y=i[4];
+        3'b101:y=i[5];
+        3'b110:y=i[6]; 
+        3'b111:y=i[7];
+    endcase
+    end
+endmodule
---
+++ DAY-3/mux_8x1_tb.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+
+module mux_8x1_tb;
+    wire y;
+    reg [7:0]i;
+    reg [2:0]s;
+    mux_8x1 dut(y,i,s);
+    initial
+    begin
+        i=8'b00000000;
+        s[0]=0;
+        s[1]=0;
+        s[2]=0;
+         #100 i=8'hca;
+          #200 i=8'h0f;
+          #500 i=8'h5a;
+          
+        #1500 $finish();
+    end
+    
+    always #100 s[0]=~s[0];
+    always #200 s[1]=~s[1];
+    always #400 s[2]=~s[2];
+
+endmodule
