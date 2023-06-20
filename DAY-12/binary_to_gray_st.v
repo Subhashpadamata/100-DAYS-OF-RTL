0f7ffba2f91e0c8900b34d6abb57cb1aedba987d
PADAMATA SUBHASH <103590978+Subhashpadamata@users.noreply.github.com>
2023-06-20 22:21:08 +0530

    Update binary_to_gray_st.v

--- DAY-12/binary_to_gray_st.v
+++ DAY-12/binary_to_gray_st.v
@@ -4,12 +4,12 @@ module binary_to_gray_st(
     output [3:0] gray,
      input [3:0] binary
     );
-    buffer b1(gray[3]=binary[3]);
+    buffer b1(gray[3],binary[3]);
         xor x1(gray[2],binary[3],binary[2]),
             x2(gray[1],binary[2],binary[1]),
             x3(gray[0],binary[1],binary[0]);
 endmodule
-module buffer (input a,output z);
+module buffer (output z,input a);
 assign z=a;
 endmodule
  
