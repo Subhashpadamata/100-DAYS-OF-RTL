280b58ee69dd16e97cf197fb9769bc2cd71fbf77
PADAMATA SUBHASH <103590978+Subhashpadamata@users.noreply.github.com>
2023-08-12 22:07:43 +0530

    Add files via upload

---
+++ DAY-54/xor_gate/xor_using_mux_tb.v
@@ -0,0 +1,17 @@
+module xor_using_mux_tb;
+    reg a, b;
+    wire y;
+    xor_using_mux uut(a,b,y);
+    initial begin
+        a = 0; b = 0;
+        #100; 
+        a = 0 ; b =1;
+        #100;
+        a = 1; b = 0;
+        #100;
+        a = 1; b = 1;
+        #100;
+        $finish;
+    end
+    initial $monitor("a=%b b=%b y=%b",a,b,y);
+endmodule
